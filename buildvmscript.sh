cf6318b3aead1558006ee5e16e6e66a919d6a157
sudheermareddy <sudheerm@sysgain.com>
2020-12-21 18:55:37 +0530

    Update buildvmscript.sh

--- buildvmscript.sh
+++ buildvmscript.sh
@@ -7,8 +7,8 @@ sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubun
 sudo apt-get update 
 sudo apt-get install -y docker-ce
 sudo usermod -aG docker $user
-wget https://raw.githubusercontent.com/sudheermareddy/test/master/giga/deploy.sh
-wget https://raw.githubusercontent.com/sudheermareddy/test/master/giga/destroy.sh
-wget https://raw.githubusercontent.com/sudheermareddy/test/master/giga/Dockerfile
-wget https://raw.githubusercontent.com/sudheermareddy/test/master/giga/backend.tf
+wget https://raw.githubusercontent.com/qloudable/gigamon/main/deploy.sh
+wget https://raw.githubusercontent.com/qloudable/gigamon/main/destroy.sh
+wget https://raw.githubusercontent.com/qloudable/gigamon/main/Dockerfile
+wget https://raw.githubusercontent.com/qloudable/gigamon/main/backend.tf
 docker build -t gigaimage/terraform:latesst .
